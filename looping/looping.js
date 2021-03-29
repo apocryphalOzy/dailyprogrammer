516bae6438f1cbf1f5fe12c6521048845d6868c8
l337hax0r <lonesealpro@gmail.com>
2021-03-29 09:39:29 -0700

    Updated for grammar

--- looping/looping.js
+++ looping/looping.js
@@ -2,8 +2,12 @@ const cats = ['Bojangles', 'BigglesWorth', 'Coissant', 'Leroy', 'Tony']
 let info = 'These are my cats names: ';
 const para = document.querySelector("p");
 
-for (let i = 0; i < cats.length; i++){
-    info += cats[i] + ", ";
+for (let i = 0; i < cats.length; i++) {
+    if (i === cats.length - 1) {
+        info += "and " + cats[i] + "."
+    } else (
+        info += cats[i] + ", "
+    )
 }
 
 para.textContent = info;
